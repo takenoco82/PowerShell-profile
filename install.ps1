2999efec73df9acf3513628d07f2d05dd88f0dbe
TAKEUCHI Daisuke <takenoco82@gmail.com>
2017-05-21 00:08:52 +0900

    インストール用のスクリプトを新規作成した。

---
+++ install.ps1
@@ -0,0 +1,9 @@
+﻿$ScriptDir = Split-Path $MyInvocation.MyCommand.Path
+
+$mklinkArgs = @()
+$mklinkArgs += [PSCustomObject]@{"source"="$(Join-Path $ScriptDir "profile.ps1")"; "target"="$($PROFILE.CurrentUserAllHosts)"}
+
+$Command = "/c cd /d `"$ScriptDir`""
+$mklinkArgs | %{ $Command += " `& mklink `"$($_.target)`" `"$($_.source)`"" }
+
+Start-Process cmd -Verb Runas -WindowStyle Hidden -ArgumentList $Command
